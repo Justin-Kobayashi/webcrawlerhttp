7c6ebfa9f45d396ca62a444405482e420a4f2558
Justin Kobayashi <justinkobayashi@cs.ucsb.edu>
2024-06-16 18:13:19 -0700

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,29 @@
 const { JSDOM } = require('jsdom')
 
+
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399){
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+}
+
 function getURLsFromHTML (htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -12,17 +36,17 @@ function getURLsFromHTML (htmlBody, baseURL) {
             } catch (err) {
                 console.log(`error with relative url: ${err.message}`)
             }
-          } else {
+        } else {
             try {
                 const urlObj = new URL(aElement.href)
                 urls.push(urlObj.href)
             } catch (err) {
                 console.log(`error with absolute url: ${err.message}`)
             }
-          }
         }
-    return urls
     }
+    return urls
+}
 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
@@ -35,6 +59,7 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
 
